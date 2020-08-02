9c7d5b6245422004876b3dfb975137ab30ce4bda
Felipe Pineiro Lopez <pineiro.felipe@gmail.com>
2020-08-02 03:59:11 -0400

    fix deployment running as react app

--- __init__.py
+++ __init__.py
@@ -7,13 +7,13 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__,static_folder='../build', static_url_path='/')
 
-    from .Config import DBConfigs
+    from .api.Config import DBConfigs
 
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
     db.init_app(app)
 
-    from .views import main
+    from .api.views import main
     app.register_blueprint(main)
 
     return app
