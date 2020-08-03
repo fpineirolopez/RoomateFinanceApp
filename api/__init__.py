15b3c60aa7184bd334df0b2413eb30f892981a2a
Felipe Pineiro Lopez <pineiro.felipe@gmail.com>
2020-08-02 23:25:02 -0400

    create db

--- api/__init__.py
+++ api/__init__.py
@@ -13,10 +13,10 @@ def create_app():
 
     db.init_app(app)
 
-    with app.app_context():
-        db.create_all()
-
     from .views import main
     app.register_blueprint(main)
+
+    with app.app_context():
+        db.create_all()
 
     return app
