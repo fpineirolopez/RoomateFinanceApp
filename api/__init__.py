7c738623b73f5b0f5e727d7bd7aec7ed3b34f0b4
Felipe Pineiro Lopez <pineiro.felipe@gmail.com>
2020-08-05 21:03:37 -0400

    added psycopg2

--- api/__init__.py
+++ api/__init__.py
@@ -9,9 +9,9 @@ def create_app():
 
     from .Config import DBConfigs
 
-    app.config['DATABASE_URL'] = os.environ.get('DATABASE_URL')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+      app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
     db.init_app(app)
 
