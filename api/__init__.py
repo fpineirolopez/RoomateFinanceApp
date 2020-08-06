46c90f6a64acf90d804c2502a7a05dd0b08698bc
Felipe Pineiro Lopez <pineiro.felipe@gmail.com>
2020-08-05 20:51:52 -0400

    fixed connection string issue V2

--- api/__init__.py
+++ api/__init__.py
@@ -9,7 +9,7 @@ def create_app():
 
     from .Config import DBConfigs
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    app.config['DATABASE_URL'] = os.environ.get('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
